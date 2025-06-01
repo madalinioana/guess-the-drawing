9a673b07a5cd691d681013a71af773836c21398b
Mădălin <madalin.andrei.ioana@gmail.com>
2025-06-01 21:22:42 +0300

    Modified socket for deployment

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3001";
+
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
+  withCredentials: true,
+});
 
 export default socket;
